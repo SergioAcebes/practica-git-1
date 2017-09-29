0cf6dae499aa53fa005ab51e1763c0811a9a8563
Sergio Acebes y Jorge Jaular <acebes9@gmail.com>
2017-09-29 11:15:00 +0100

    indica tambien mi edad

--- DAM.java
+++ DAM.java
@@ -1,7 +1,7 @@
 public class DAM{
  public static void main(String args[]){
 
-	System.out.println("Jorge Jaular Lasaga, Sergio Acebes Ponga");
-
+	System.out.println("Sergio Acebes Ponga");
+	System.out.println("20 años");
  }
 }
